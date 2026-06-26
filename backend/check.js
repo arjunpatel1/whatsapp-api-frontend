e29413036a6319334af3148b0468ab8db5ad264a
GangabalajiGuggilla <Guggilla.Balaji@myteknoland.net>
2026-06-26 15:19:11 +0530

    Remove unused and duplicate files

--- backend/check.js
+++
@@ -1,6 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('data.db');
-db.all('SELECT * FROM templates', (err, rows) => {
-  console.log(JSON.stringify(rows, null, 2));
-  db.close();
-});
